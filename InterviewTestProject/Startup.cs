7f725f09be56c578d08889a9c9ff1e733a748514
Yaroslav <ykoste@softserveinc.com>
2022-01-31 14:52:33 +0200

    rename solution name

--- InterviewTestProject/Startup.cs
+++ InterviewTestProject/Startup.cs
@@ -1,4 +1,4 @@
-using InverviewProject.Services;
+using InterviewTestProject.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace InverviewProject
+namespace InterviewTestProject
 {
     public class Startup
     {
